bfaf9f848c2f44aa9a74942e85f4bf73b9d84ef3
SahGW <soiamhon@gmail.com>
2022-04-22 01:17:31 +0700

    signin & signup

---
+++ client/actions/auth.js
@@ -0,0 +1,115 @@
+import fetch from "isomorphic-fetch";
+import cookie from "js-cookie";
+import { API } from "../config";
+
+export const signup = (user) => {
+    return fetch(`${API}/auth/signup`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((err) => console.log(err));
+};
+
+export const signin = (user) => {
+    return fetch(`${API}/auth/signin`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((err) => console.log(err));
+};
+
+export const signout = (next) => {
+    removeCookie("token");
+    removeLocalStorage("user");
+    next();
+
+    return fetch(`${API}/auth/signout`, {
+        method: "GET",
+    })
+        .then((response) => {
+            console.log("signout success");
+        })
+        .catch((err) => console.log(err));
+};
+
+// set cookie
+export const setCookie = (key, value) => {
+    if (typeof window === "undefined") {
+        cookie.set(key, value, {
+            expires: 1,
+        });
+    }
+};
+
+export const removeCookie = (key) => {
+    if (typeof window === "undefined") {
+        cookie.remove(key, {
+            expires: 1,
+        });
+    }
+};
+// get cookie
+export const getCookie = (key) => {
+    if (typeof window === "undefined") {
+        return cookie.get(key);
+    }
+};
+// localstorage
+export const setLocalStorage = (key, value) => {
+    if (typeof window === "undefined") {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+};
+
+export const removeLocalStorage = (key) => {
+    if (typeof window === "undefined") {
+        localStorage.removeItem(key);
+    }
+};
+// autheticate user by pass data to cookie and localstorage
+export const authenticate = (data, next) => {
+    setCookie("token", data.token);
+    setLocalStorage("user", data.user);
+    next();
+};
+
+export const isAuth = () => {
+    if (typeof window === "undefined") {
+        const cookieChecked = getCookie("token");
+        if (cookieChecked !== "undefined") {
+            if (
+                typeof window !== "undefined" &&
+                localStorage.getItem("user") !== "undefined"
+            ) {
+                return JSON.parse(localStorage.getItem("user"));
+            } else {
+                return false;
+            }
+        }
+    }
+};
+
+export const updateUser = (user, next) => {
+    if (typeof window === "undefined") {
+        if (localStorage.getItem("user")) {
+            let auth = JSON.parse(localStorage.getItem("user"));
+            auth = user;
+            localStorage.setItem("user", JSON.stringify(auth));
+            next();
+        }
+    }
+};
---
+++ client/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+const index = () => {
+    return (
+        <Layout>
+            <h2>Index page</h2>
+        </Layout>
+    );
+};
+
+export default index;
